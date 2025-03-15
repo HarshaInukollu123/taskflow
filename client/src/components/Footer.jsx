e3ebfa9de858be67943c8b8850bf25d5166147e4
HarshaInukollu <harshainukollu3@gmail.com>
2025-03-14 23:06:20 -0500

    Added FrontendSetup

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+       <footer className="bg-gray-800 p-4 text-white text-center">
+         <p>&copy; 2023 TaskFlow. All rights reserved.</p>
+       </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="container mx-auto p-4">
+        <h2 className="text-xl font-bold mb-4">Welcome to TaskFlow</h2>
+        <p>Manage your tasks efficiently with TaskFlow.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
