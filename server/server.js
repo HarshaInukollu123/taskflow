1bb52904b1bbf8e717b17894baadae2356b0ef1f
HarshaInukollu123 <harshainukollu3@gmail.com>
2025-05-13 20:51:40 -0500

    minor fix

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-frontend-url.com'],
+  origin: ['http://localhost:5173', 'https://taskflow-ten-gamma.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
